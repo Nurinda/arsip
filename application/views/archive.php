96a8f7e3be7fb834409354238363a39eb502cad4
titoanugerah <titoanugerah@student.undip.ac.id>
2019-06-23 15:30:47 +0700

    Added play feature on guest page

--- application/views/archive.php
+++ application/views/archive.php
@@ -19,7 +19,7 @@
               <td align="center"><?php echo $item->title; ?> </td>
               <td align="center"><?php echo $item->institute; ?></td>
               <td align="center"><?php if($item->status==1){echo 'Dipinjam';} else {echo 'Tersedia';} ?></td>
-              <td align="center"><a href="<?php echo base_url('detailArchive/'.$item->id);?>" class="btn btn-warning">Detail</a> </td>
+              <td align="center"><a href="<?php echo base_url('detailArchive/'.$item->id);?>" class="btn btn-warning"><i class="material-icons">book</i>&nbsp;&nbsp; Detail</a><button type="button" class="btn btn-success" data-toggle="modal" data-target="#viewVideo<?php echo $item->id; ?>"><i class="material-icons">play_arrow</i>&nbsp;&nbsp; Lihat Video</button> </td>
             </tr>
             <?php $no++; endforeach; ?>
           </table>
@@ -55,4 +55,28 @@
     </div>
   </div>
 
-  
+  <?php $no = 1; foreach ($content['video'] as $item):?>
+
+  <div class="modal fade" id="viewVideo<?php echo $item->id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <form  method="post">
+        <div class="modal-content">
+
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Lihat Video</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <iframe width="100%" height="250px" src="https://www.youtube.com/embed/<?php echo $item->link; ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+
+          <div class="modal-footer modal-danger">
+            <button type="button" class="btn btn-grey" data-dismiss="modal">Kembali</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+<?php endforeach; ?>
